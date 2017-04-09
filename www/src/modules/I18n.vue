8cb6262ff5aa7f7d0398a77fba16b9517d84db0a
袁臻 <f0rb@163.com>
2017-04-09 13:44:28 +0800

    I18n.vue给table设置最大高度

--- www/src/modules/I18n.vue
+++ www/src/modules/I18n.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="position:relative" ref="out">
     <div class="mb-2">
       <a class="btn btn-secondary" :href="openapi + '/openapi/i18n/' + group + '.xlsx'" v-t>导出Excel</a>
       <b-btn v-t>打包JSON格式</b-btn>
@@ -8,15 +8,15 @@
       <b-btn class="btn-success float-right ml-2" v-t>添加标签</b-btn>
       <b-btn class="btn-success float-right" v-t>添加语种</b-btn>
     </div>
-    <section>
-      <table class="table table-hover" style="margin-bottom:0">
+    <section style="overflow-y:auto" :style="{maxHeight: (tableHeight) + 'px'}">
+      <table class="table table-hover" style="margin-bottom:0;">
         <thead>
         <tr class="text-center">
           <th>#</th>
           <th v-for="(v, k) in crud.p.list[0]"><t>{{k == 'value' ? '默认值' : k}}</t></th>
         </tr>
         </thead>
-        <tbody>
+        <tbody style="background-color: #f7f7f9;">
         <tr v-for="(group, $index) in crud.p.list">
           <td>{{$index+1}}</td>
           <td v-for="(v, k) in crud.p.list[0]">{{group[k]}}</td>
@@ -25,13 +25,13 @@
       </table>
     </section>
     <footer>
-      <div v-if="crud.p.pages" class="flow page-ul">
-        <div class="btn-group">
+      <div v-if="crud.p.pages" class="flow page-footer">
+        <div class="btn-group btn-group-sm">
           <button class="btn btn-secondary" @click="crud.p.first"><i class="fa fa-step-backward"></i></button>
           <button class="btn btn-secondary" @click="crud.p.prev"><i class="fa fa-backward"></i></button>
-          <b-dropdown :text="crud.p.page + ' / ' + crud.p.pages" dropup class="page-number">
-            <b-dropdown-item v-for="(s, $i) in crud.p.pages" :key="$i">
-              <a @click="crud.p.goto($i+1)">{{$i+1}} / {{crud.p.pages}}</a>
+          <b-dropdown size="sm" :text="crud.p.page + ' / ' + crud.p.pages" dropup class="page-number">
+            <b-dropdown-item v-for="(s, $i) in crud.p.pages" :key="$i" @click="crud.p.goto($i+1)">
+              {{$i+1}} / {{crud.p.pages}}
             </b-dropdown-item>
           </b-dropdown>
           <button class="btn btn-secondary" @click="crud.p.next"><i class="fa fa-forward"></i></button>
@@ -40,8 +40,8 @@
         </div>
         <div class="pull-right">
           <span class="hidden-xs">共有{{crud.p.total}}条数据，显示第{{crud.p.from}}条到第{{crud.p.to}}条，每页</span>
-          <b-dropdown :text="crud.p.limit + ''" dropup :right="true" class="page-size">
-            <b-dropdown-item v-for="(s, $i) in [5,10,20,30,50]" :key="$i" class="text-right"><a @click="crud.p.size(s)">{{s}}</a></b-dropdown-item>
+          <b-dropdown size="sm" :text="crud.p.limit + ''" dropup :right="true" class="page-size">
+            <b-dropdown-item v-for="(s, $i) in [5,10,20,30,50]" :key="$i" @click="crud.p.size(s)" class="text-right">{{s}}</b-dropdown-item>
           </b-dropdown>
           条
         </div>
@@ -50,18 +50,31 @@
   </div>
 </template>
 <style lang="scss">
-  .page-size {
-    .dropdown-toggle, .dropdown-menu {
-      min-width:85px;
+  .page-footer {
+    border-top:1px #eee solid;
+    padding-top:6px;
+    position:absolute;
+    left:0;
+    right:0;
+    bottom:-40px;
+
+    .page-size {
+      .dropdown-toggle, .dropdown-menu {
+        min-width:85px;
+      }
     }
-  }
-  .page-number {
-    .dropdown-toggle, .dropdown-menu {
-      min-width:115px;
-    }
-    .dropdown-menu {
-      max-height: 200px;
-      overflow-y:scroll;
+    .page-number {
+      .dropdown-toggle, .dropdown-item, .dropdown-menu  {
+        min-width:80px;
+      }
+      .dropdown-item {
+        padding:3px 0.75rem;
+        /*text-align:right;*/
+      }
+      .dropdown-menu {
+        max-height:200px;
+        overflow-y:scroll;
+      }
     }
   }
 </style>
@@ -73,9 +86,12 @@
   export default {
     data () {
       return {
+        tableHeight: 200,
         openapi: Cons.apiHost,
         group: this.$route.params.group,
-        crud: null
+        crud: {
+          p: {}
+        }
       }
     },
     components: {},
@@ -83,6 +99,13 @@
       // 组件创建完后获取数据，
       // 此时 data 已经被 observed 了
       this.init()
+    },
+    mounted () {
+      this.handleTableHeight()
+      window.addEventListener('resize', this.handleTableHeight)
+    },
+    beforeDestroy: function () {
+      window.removeEventListener('resize', this.handleTableHeight)
     },
     watch: {
       // 如果路由有变化，会再次执行该方法
@@ -104,6 +127,9 @@
             Util.handleFailure(data)
           }
         })
+      },
+      handleTableHeight () {
+        this.tableHeight = this.$el.parentElement.clientHeight - 50 - 60 - 45
       }
     }
   }
