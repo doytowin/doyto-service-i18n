76356c95148345b4f82aabf00b4e0d5580cc7334
袁臻 <f0rb@163.com>
2018-04-15 15:13:26 +0800

    Jackson配置

--- src/main/java/win/doyto/i18n/configurer/WebMvcConfigurer.java
+++ src/main/java/win/doyto/i18n/configurer/WebMvcConfigurer.java
@@ -1,52 +1,114 @@
 package win.doyto.i18n.configurer;
 
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import javax.annotation.Resource;
+import java.util.TimeZone;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 /**
  * WebMvcConfigurer
  *
  * @author f0rb on 2017-04-23.
  */
+@Slf4j
 @Configuration
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        //registry.addMapping("/openapi/**").allowedOrigins("*");
-        //registry.addMapping("/api/**").allowedOrigins("http://localhost:9017");
-        //registry.addMapping("/api/i18n/i18n/zh_CN").allowedOrigins("http://localhost:8080");
+    @Bean
+    public HandlerInterceptor logInterceptor() {
+        return new HandlerInterceptorAdapter() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                log.info("Request:URI {} URL {}", request.getRequestURI(), request.getRequestURL());
+                return true;
+            }
+        };
     }
 
-    @Resource
-    public void setMappingJackson2HttpMessageConverter(MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter) {
-        List<MediaType> supportedMediaTypes=new ArrayList<>();
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor()).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("classpath:/favicon.ico");
+        super.addResourceHandlers(registry);
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof ByteArrayHttpMessageConverter) {
+                configByteArrayHttpMessageConverter((ByteArrayHttpMessageConverter) converter);
+            }
+            if (converter instanceof StringHttpMessageConverter) {
+                configStringHttpMessageConverter((StringHttpMessageConverter) converter);
+            }
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                configMappingJackson2HttpMessageConverter((MappingJackson2HttpMessageConverter) converter);
+            }
+        }
+    }
+
+    private void configByteArrayHttpMessageConverter(ByteArrayHttpMessageConverter converter) {
+        converter.setSupportedMediaTypes(Arrays.asList(
+                MediaType.valueOf("image/*"),
+                MediaType.APPLICATION_OCTET_STREAM
+        ));
+    }
+
+    private void configStringHttpMessageConverter(StringHttpMessageConverter converter) {
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        converter.setSupportedMediaTypes(Arrays.asList(
+                MediaType.TEXT_PLAIN
+                , MediaType.TEXT_HTML
+                , MediaType.APPLICATION_JSON
+        ));
+    }
+
+
+    private void configMappingJackson2HttpMessageConverter(MappingJackson2HttpMessageConverter converter) {
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
         supportedMediaTypes.add(MediaType.APPLICATION_JSON);
         supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
         supportedMediaTypes.add(MediaType.valueOf("application/*+json"));
-        supportedMediaTypes.add(MediaType.valueOf("application/*+json;charset=UTF-8"));
-        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(supportedMediaTypes);
+        supportedMediaTypes.add(MediaType.TEXT_HTML);
+        converter.setSupportedMediaTypes(supportedMediaTypes);
 
-        ObjectMapper objectMapper = mappingJackson2HttpMessageConverter.getObjectMapper();
+        ObjectMapper objectMapper = converter.getObjectMapper();
         objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
         objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         objectMapper.configure(JsonParser.Feature.IGNORE_UNDEFINED, true);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        //objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        objectMapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
     }
 
 }
