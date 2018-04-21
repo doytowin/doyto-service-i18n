49bf9a214c73462a014bc64d9c66925bf463f145
袁臻 <f0rb@163.com>
2018-04-21 16:33:15 +0800

    前端单独开发

--- www/config/prod.env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  url: '"http://i18n.doyto.win/"',
-  apiHost: '"http://api.doyto.win/"',
-}
--- www/src/components/Cons.js
+++
@@ -1,5 +0,0 @@
-
-export default {
-  url: process.env.url,
-  apiHost: process.env.apiHost
-}
