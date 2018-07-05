e7a00928ef154090fa20f9f094ed6ab3fe9a7b40
袁臻 <f0rb@163.com>
2018-07-05 18:03:10 +0800

    [doyto-service-i18n]+HandlerMappingIntrospector

--- src/main/java/win/doyto/i18n/configurer/WebMvcConfigurer.java
+++ src/main/java/win/doyto/i18n/configurer/WebMvcConfigurer.java
@@ -4,11 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 import win.doyto.web.spring.CommonWebMvcConfiguration;
 
 /**
@@ -36,5 +39,10 @@ public class WebMvcConfigurer extends CommonWebMvcConfiguration {
         registry.addInterceptor(logInterceptor()).addPathPatterns("/**");
     }
 
+
+    @Bean @Lazy
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector(ApplicationContext context) {
+        return new HandlerMappingIntrospector(context);
+    }
 
 }
